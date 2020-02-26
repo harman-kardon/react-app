5677a584d168f697ad30553e211232b1dd0d0fca
Mathieu <ddmunky@gmail.com>
2020-02-26 15:19:50 +0100

    Updates

---
+++ precache-manifest.368591b47211bdbb89057a9582cc502b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4c2f5512e674ba396b5a8efa223510",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "533d474717bfa592f083",
+    "url": "/react-app/static/css/main.84681a68.chunk.css"
+  },
+  {
+    "revision": "f18ec448c3ab67b3b179",
+    "url": "/react-app/static/js/2.9e4b70aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-app/static/js/2.9e4b70aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533d474717bfa592f083",
+    "url": "/react-app/static/js/main.8f8cef3b.chunk.js"
+  },
+  {
+    "revision": "161f00e5262ef4045442",
+    "url": "/react-app/static/js/runtime-main.e11c5356.js"
+  }
+]);
